5f06228d35d7461d3aebc5f2f8bc581f49a3c07b
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-18 15:01:33 -0500

    initial upload of timer2.js

--- timer1.js
+++ timer1.js
@@ -2,13 +2,12 @@ const alarmTimes = process.argv;
 
 const alarmClock = function(alarms) {
   let delay = 0;
-  for (const alarm of alarms.slice(2)) {
-    if (alarm > 0 || !isNaN(alarm)) {
-      setTimeout(() => {
-        process.stdout.write('ALARM!');
-      },alarm * 1000 + delay);
-      delay += alarm * 1000;
-    }
+  for (const alarmTime of alarms.slice(2)) {
+    delay += alarmTime * 1000;
+    setTimeout(() => {
+      process.stdout.write('ALARM!');
+    },delay);
+    
   }
 };
 
