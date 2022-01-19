c4f6e93b80e276e2aec1bd8989f451504a7fce91
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-19 18:40:52 -0500

    adding timer2.js functionality

--- timer2.js
+++ timer2.js
@@ -1,21 +1,33 @@
-const alarmTimes = process.argv;
 
-const alarmClock = function(alarms) {
-  let delay = 0;
-  for (const alarmTime of alarms.slice(2)) {
-    delay += alarmTime * 1000;
-    setTimeout(() => {
-      process.stdout.write('ALARM!');
-    },delay);
-    
-  }
+
+const alarmClock = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+
+  stdin.on('data', (key) => {
+    if (key.toUpperCase() === 'B') {
+      process.stdout.write('ALARM!\n');
+    }
+
+    if (Number(key) >= 1 || Number(key) <= 9) {
+      process.stdout.write(`Alarm set for ${key} seconds from now...\n`);
+      setTimeout(() => {
+        process.stdout.write('ALARM!\n');
+      },Number(key) * 1000);
+    }
+
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
 };
 
-alarmClock(alarmTimes);
+// The user can press b and it should beep right away
+// The user can input any number from 1 to 9 and it should
+//     immediately output "setting timer for x seconds...", and
+//     beep after that number of seconds has passed
+// The user can use ctrl + c to exit the program, at which point the program should say "Thanks for using me, ciao!" before terminating
 
 
-// user input: b beeps right away
-// can input numbers 1 to 9:
-// - will say 'setting timer for X seconds...'
-// can ctrl+c to exit program should say:
-// 'Thanks for using me, ciao!' then exit
+alarmClock();
